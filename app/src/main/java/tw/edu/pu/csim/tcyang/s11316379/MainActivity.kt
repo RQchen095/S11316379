d9aa8fdd5f6109f6949b7f7e8a1544de50763ec7
RQchen095 <chenruoqi950@gmail.com>
2025-12-01 14:33:31 +0800

    v1_陳若綺

--- app/src/main/java/tw/edu/pu/csim/tcyang/s11316379/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/s11316379/MainActivity.kt
@@ -12,11 +12,37 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.csim.tcyang.s11316379.ui.theme.S11316379Theme
+import android.content.pm.ActivityInfo
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        // 1. 強制螢幕為直式 (PORTRAIT)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+        // 2. 隱藏上方狀態列及下方巡覽列
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+            window.insetsController?.let {
+                it.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    )
+        }
+
+        // 移除 enableEdgeToEdge()，因為我們要完全隱藏系統列
+
         setContent {
             S11316379Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
